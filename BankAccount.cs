5313e0efef7dc8419ce352f87cc20c4b03cc094d
Jakub Duma <108173609+JakubDuma@users.noreply.github.com>
2022-11-13 21:09:01 +0100

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBank
+{
+    class BankAccount
+    {
+        private List<Transaction> AllTransactions = new List<Transaction>();
+
+        private static UInt32 accountNumberSeed = 23232323;
+        public UInt32 Number { get; }
+        public string Owner { get; set; }
+        private decimal credit;
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get {
+                decimal transactionSum = 0;
+                foreach (var transaction in AllTransactions)
+                {
+                    transactionSum += transaction.Amount;
+                }
+                return transactionSum + balance;
+            }
+            set { balance = value; }
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed++;
+            Console.WriteLine($"Created new account with balance: {initialBalance}");
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            Console.WriteLine($"{amount} has been deposited.");
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "You cannot deposit a negative amount.");
+            }
+
+            Transaction deposit = new Transaction(amount, date, note);
+            AllTransactions.Add(deposit);
+
+        }
+
+        public void MakeWithdraw(decimal amount, DateTime date, string note)
+        {
+            Console.WriteLine($"{amount} has been paid out.");
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "You cannot withdraw a negative amount");
+            } else if (amount > balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Insufficient balance in the account.");
+            }
+            Transaction withdraw = new Transaction(-amount, date, note);
+            AllTransactions.Add(withdraw);
+
+        }
+
+        public void ListTransactionHistory()
+        {
+            Console.WriteLine("Amount         Date          Description");
+            foreach (var Transaction in AllTransactions)
+            {
+                Console.Write(Transaction.Amount + "  ");
+                Console.Write(Transaction.Date + "  ");
+                Console.WriteLine(Transaction.Note);
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("\nSelect action: 1 - Deposit, 2 - Withdraw, 3 - Balance, 4 - Transaction history, 5 - Credit, 6 - Credit payment, 7 - Actual credit, 8 - Exit");
+        }
+        public void Credit(decimal amount, DateTime date, string note)
+        {
+
+
+            if (amount > 1000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The maximum amount of credit is: 1000");
+            }
+            else if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The amount of credit must not be negative");
+            }
+            Console.WriteLine($"Amount {amount} has been added to balance.");
+            Transaction creditt = new Transaction(amount, date, note);
+                AllTransactions.Add(creditt);
+                credit += amount;
+            
+        }
+    
+        public void creditPayment(decimal amount, DateTime date, string note)
+        {
+            if (amount > credit)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The amount of credit is less than the amount paid");
+            }
+            else if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The amount deposited must not be negative");
+            }
+              Console.WriteLine($"Amount {amount} has been deposited.");
+              Transaction credpay = new Transaction(amount = -amount, date, note);
+                AllTransactions.Add(credpay);
+            credit -= amount;
+
+        }
+        public void showCredit()
+        {
+            Console.WriteLine(credit);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace SimpleBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                playWithAccount();          
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something went wrong: {e.Message}");
+                Console.ReadKey();
+            }
+        }
+
+
+        static public void playWithAccount()
+        {
+            Console.WriteLine("Enter your name:");
+            var Name  = Console.ReadLine();
+            Console.WriteLine("Enter the amount of your first deposit: ");
+            int Money = int.Parse(Console.ReadLine());
+            BankAccount bankAccount = new BankAccount(Name, Money);
+            do
+            {
+                
+                bankAccount.DisplayInfo();
+                int selection = int.Parse(Console.ReadLine());
+                if (selection == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter deposit amount: ");
+                    int deposit = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Deposit description (If empty, press Enter)");
+                    string depoDescr = Console.ReadLine();
+                    bankAccount.MakeDeposit(deposit, DateTime.Now, depoDescr);
+
+                } else if (selection == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter withdraw amount: ");
+                    int withdraw = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Withdraw description (If empty, press Enter)");
+                    string withDescr = Console.ReadLine();
+                    bankAccount.MakeWithdraw(withdraw, DateTime.Now, withDescr);
+
+                } else if (selection == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine(bankAccount.Balance);
+
+                } else if (selection == 4)
+                {
+                    Console.Clear();
+                    bankAccount.ListTransactionHistory();
+
+                } else if (selection == 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter credit amount: ");
+                    int cre = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Credit description (If empty, press Enter)");
+                    string creDescr = Console.ReadLine();
+                    bankAccount.Credit(cre, DateTime.Now, creDescr);
+
+                } else if (selection == 6)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter credit payment amount: ");
+                    int payment = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Payment description (If empty, press Enter)");
+                    string paymDescr = Console.ReadLine();
+                    bankAccount.creditPayment(payment, DateTime.Now, paymDescr);
+                } else if (selection == 7)
+                {
+                    bankAccount.showCredit();
+                }
+                else if (selection == 8)
+                {
+                    break;
+                }
+            } while(true);
+        }
+
+    }
+}
